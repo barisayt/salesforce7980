196efdb7407dddaedc0e7bdeb67086bb8a595839
Baris Aytepe <netshell0@gmail.com>
2022-06-18 15:50:57 -0400

    newo

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     // Welcome dear salesforce developers
     // Dear friends this is second version
     // Everyone git is easy to understand
+    // Here I will create a new command here for master Branch
 
 }
 
