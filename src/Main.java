30dd3d2754b25171d75b07f41e63a2de713b0a37
Baris Aytepe <netshell0@gmail.com>
2022-06-18 15:39:07 -0400

    New Version

--- src/Main.java
+++ src/Main.java
@@ -5,3 +5,4 @@ public class Main {
 // Welcome dear salesforce developers
 // Dear friends this is second version
 // Everyone git is easy to understand
+// I will create a new command here for master branch
